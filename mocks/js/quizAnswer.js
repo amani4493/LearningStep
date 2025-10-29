d7dfedb2792ed4f2f821abc08a3539537417e690
Amani999 <shinichi2015gm@gmail.com>
2025-10-29 19:21:40 +0900

    brief commit.

---
+++ mocks/js/quizAnswer.js
@@ -0,0 +1,23 @@
+const mockQuiz = [
+    {
+        id: 1,
+        quizText: "ハロゲン化銀のうち、以下の画像左、白色の物質は何か",
+        choices: [
+            { id: "A", label: "Agcl" },
+            { id: "B", label: "NaCl" },
+            { id: "C", label: "HCl"},
+            { id: "D", label: "NaOH"}
+        ],
+        quizImg: "images/quiz_img_mock.jpg"
+    },
+    {
+        id: 2,
+        quizText: "AgNO₃＋NaClで白い沈殿ができました。その沈殿は何でしょう？",
+        choices: [
+            { id: "A", label: "Agcl" },
+            { id: "B", label: "NaCl" },
+            { id: "C", label: "HCl"},
+            { id: "D", label: "NaOH"}
+        ]
+    }
+];
